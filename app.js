5d172df5e6eb0f68cd3e62f1e418f74843898884
Mehmet Okan Özcan <mehmetokanozcan@192.168.1.34>
2022-03-18 17:13:34 +0300

    fix

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const isLogin = require('./_helper/index')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var exportRouter = require('./routes/export');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,17 +20,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', isLogin, indexRouter);
+app.use('/export', exportRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((err, req, res, next) => {
+  res.status(err.status);
+  res.send({
+    message: err.message,
+    status: err.status,
+  })
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  res.setHeader('Content-Type', 'application/json');
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
