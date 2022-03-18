05dce8ab1c793b16ff9e23fc78c1355d059bf3a6
Mehmet Okan Özcan <mehmetokanozcan@192.168.1.34>
2022-03-18 17:14:32 +0300

    fix

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// See https://prettier.io/docs/en/index.html for more about .prettierrc.js files.
+
+module.exports = {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+    endOfLine: 'auto', // 'lf'
+    bracketSpacing: true, // { foo: bar }
+    bracketSameLine: true,
+};
